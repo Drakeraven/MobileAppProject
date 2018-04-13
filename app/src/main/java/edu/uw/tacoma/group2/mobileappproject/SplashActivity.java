a6af11a503c7a18590af3c2519f93cab9c693aa4
Stephanie D <stephanie_day8@msn.com>
2018-04-13 13:31:12 -0700

    Initial commit

---
+++ app/src/main/java/edu/uw/tacoma/group2/mobileappproject/SplashActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.tacoma.group2.mobileappproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
