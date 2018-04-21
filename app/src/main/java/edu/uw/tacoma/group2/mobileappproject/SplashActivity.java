821f39fcaf54fdfc6f8e3d901da1693d99cbd506
Stephanie D <stephanie_day8@msn.com>
2018-04-21 13:21:18 -0700

    Friend/Group Layout complete

--- app/src/main/java/edu/uw/tacoma/group2/mobileappproject/SplashActivity.java
+++ app/src/main/java/edu/uw/tacoma/group2/mobileappproject/SplashActivity.java
@@ -1,7 +1,9 @@
 package edu.uw.tacoma.group2.mobileappproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    //TODO:Remove when have proper connection to the Friend/List Group
+    public void openFriendGroup(View view) {
+        Intent fGroup = new Intent(this, FriendGroupActivity.class);
+        startActivity(fGroup);
     }
 }
