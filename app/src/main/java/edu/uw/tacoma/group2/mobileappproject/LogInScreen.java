6ee21fd3efeca4a2eba1f03484465acac6274114
SaltyBard <cbr.work@hotmail.com>
2018-04-30 21:11:30 -0700

    Facebook sign in/log out is currently working at the bottom of the splash page

--- app/src/main/java/edu/uw/tacoma/group2/mobileappproject/LogInScreen.java
+++ app/src/main/java/edu/uw/tacoma/group2/mobileappproject/LogInScreen.java
@@ -9,6 +9,7 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -38,8 +39,8 @@ public class LogInScreen extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 setResult(RESULT_OK);
-                AccessToken token = AccessToken.getCurrentAccessToken();
-                Log.i(TAG, "onSuccess: " + token);
+                Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getUserId());
+                Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getToken());
                 //Intent i = new Intent(LogInScreen.this, SplashActivity.class);
                 //startActivity(i);
                 finish();
@@ -54,9 +55,35 @@ public class LogInScreen extends AppCompatActivity {
             @Override
             public void onError(FacebookException e) {
                 // Handle exception
+                Log.i(TAG, "onError: ");
             }
         });
 
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getUserId());
+                        Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getToken());
+                    }
 
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.i(TAG, "onError: ");
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/edu/uw/tacoma/group2/mobileappproject/SplashActivity.java
+++ app/src/main/java/edu/uw/tacoma/group2/mobileappproject/SplashActivity.java
@@ -12,13 +12,23 @@ import android.view.View;
 import android.widget.Button;
 
 import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String EMAIL = "email";
+    private static final String USER_POSTS = "user_posts";
+
+    private CallbackManager mCallbackManager;
 
     private static final String TAG = "Splash Activity";
 
@@ -45,13 +55,71 @@ public class SplashActivity extends AppCompatActivity {
 
         // If MainActivity is reached without the user being logged in, redirect to the Login
         // Activity
-        if (AccessToken.getCurrentAccessToken() == null) {
+        /*if (AccessToken.getCurrentAccessToken() == null) {
             Log.i(TAG, "Facebook Login Token: NULL");
             Intent loginIntent = new Intent(this, LogInScreen.class);
             startActivity(loginIntent);
 
-        }
+        }*/
 
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginButton mLoginButton = findViewById(R.id.login_button);
+
+        // Set the initial permissions to request from the user while logging in
+        mLoginButton.setReadPermissions(Arrays.asList(EMAIL, USER_POSTS));
+
+        // Register a callback to respond to the user
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                setResult(RESULT_OK);
+                Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getUserId());
+                Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getToken());
+                //Intent i = new Intent(LogInScreen.this, SplashActivity.class);
+                //startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                // Handle exception
+                Log.i(TAG, "onError: ");
+            }
+        });
+
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getUserId());
+                        Log.i(TAG, "onSuccess: " + loginResult.getAccessToken().getToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.i(TAG, "onError: ");
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     //TODO:Remove when have proper connection to the Friend/List Group
