23683f430f7a22f9a0a486c56ca228635711448c
Exeter <stephanie_Day8@msn.com>
2018-05-31 15:35:14 -0700

    Removed setting menus

--- app/src/main/java/edu/uw/tacoma/group2/mobileappproject/hangout/HangoutActivity.java
+++ app/src/main/java/edu/uw/tacoma/group2/mobileappproject/hangout/HangoutActivity.java
@@ -128,9 +128,6 @@ public class HangoutActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
